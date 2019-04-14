976233dbf46e33642e80e17d9e1a70621b9751eb
vikas-kr-gupta <vikasgupta.net@live.com>
2019-04-14 13:22:46 +0530

    - lock vs await

---
+++ LockVsAwait/LockVsAwait/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace LockVsAwait
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(296, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Process Item (Tasks)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 50);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(278, 186);
+            this.listView1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Items Processing Status";
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(476, 50);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(295, 186);
+            this.listView2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(473, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "ThreadPool Status";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(296, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Process Item (Thread)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 454);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "AsyncVsLock";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listView2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ LockVsAwait/LockVsAwait/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LockVsAwait
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var x = ThreadPool.SetMaxThreads(10, 10);
+        }
+        int _sharedVariable;
+        private readonly object _objLock = new object();
+        int workerThreads = 0;
+        int complitionPortThreads = 0;
+
+        private  async void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            listView2.Items.Clear();
+
+            listView2.Items.Add("Before creating T1 and T2");
+
+            listView2.Items.Add($"Worker Threads:{workerThreads}, Complition Port Threads:{complitionPortThreads}");
+
+            Task t1 = new Task(() => ProcessItem(()=> UpdateUIThread()));
+            t1.Start();
+            Task t2 = new Task(() => ProcessItem(() => UpdateUIThread()));
+            t2.Start();
+
+            ThreadPool.GetAvailableThreads(out workerThreads, out complitionPortThreads);
+            listView2.Items.Add("After creating T1 and T2");
+            listView2.Items.Add($"Worker Threads:{workerThreads}, Complition Port Threads:{complitionPortThreads}");
+
+            listView2.Items.Add("Waiting for T1 and T2");
+
+            await t1;
+            await t2;
+           
+
+            ThreadPool.GetAvailableThreads(out workerThreads, out complitionPortThreads);
+            listView2.Items.Add("After T1 and T2 finishes");
+
+            listView2.Items.Add($"Worker Threads:{workerThreads}, Complition Port Threads:{complitionPortThreads}");
+
+        }
+        private void UpdateUIThread()
+        {
+            this.listView1.Invoke((MethodInvoker)delegate
+            {
+                listView1.Items.Add(_sharedVariable.ToString());
+            });
+        }
+
+        private void ProcessItem(Action updateUIThread)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(10);
+                lock (_objLock)
+                {
+                    _sharedVariable++;
+                    updateUIThread();
+                }
+            }
+        }
+        private void ProcessItem()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(10);
+                lock (_objLock)
+                {
+                    _sharedVariable++;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            listView2.Items.Clear();
+            listView2.Items.Add("Before creating T1 and T2");
+
+            listView2.Items.Add($"Worker Threads:{workerThreads}, Complition Port Threads:{complitionPortThreads}");
+
+            Thread t1 = new Thread(new ThreadStart(ProcessItem));
+            Thread t2 = new Thread(new ThreadStart(ProcessItem));
+
+           
+
+            ThreadPool.GetAvailableThreads(out workerThreads, out complitionPortThreads);
+            listView2.Items.Add("After creating T1 and T2");
+
+            listView2.Items.Add($"Worker Threads:{workerThreads}, Complition Port Threads:{complitionPortThreads}");
+            listView2.Items.Add("Waiting for T1 and T2");
+
+          
+            t1.Start();
+            listView2.Items.Add("After runing T1 ");
+            listView2.Items.Add($"Worker Threads:{workerThreads}, Complition Port Threads:{complitionPortThreads}");
+            t1.Join();
+            listView2.Items.Add("After runing T2 ");
+            listView2.Items.Add($"Worker Threads:{workerThreads}, Complition Port Threads:{complitionPortThreads}");
+            t2.Start();
+            t2.Join();
+
+            ThreadPool.GetAvailableThreads(out workerThreads, out complitionPortThreads);
+            listView2.Items.Add("After T1 and T2 finishes");
+
+            listView2.Items.Add($"Worker Threads:{workerThreads}, Complition Port Threads:{complitionPortThreads}");
+            UpdateUIThread();
+        }
+    }
+}
